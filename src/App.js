2939e4aeb61334ca025b9a63b37c42c17e652a3c
Parav <parabbiswal@gmail.com>
2020-08-07 00:03:23 +0530

    second component completed with use of props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Navbar,NavbarBrand,} from 'reactstrap';
+import {DISHES} from './shared/dishes';
+import Menu from './components/Menu'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    }
+  }
+  render(){
+    return(
+      <div>
+        <Navbar dark color="secondary">
+          <div className ="container">
+            <NavbarBrand>[P|B]</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}></Menu>
+      </div>
+    )
+  } 
 }
 
 export default App;
